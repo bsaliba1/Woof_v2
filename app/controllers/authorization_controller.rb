02b49cc2fdfc5d92f1cccc70adef3321f2367997
Baptiste Saliba <bsaliba@justworks.com>
2019-06-27 15:39:35 -0400

    renamed method

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,4 +1,4 @@
 class AuthorizationController < ApplicationController
-  def initial
+  def sign_in
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'dashboard#home'
-  get '/sign_in' => 'authorization#initial', as: 'sign_in'
+  get '/sign_in' => 'authorization#sign_in', as: 'sign_in'
   get '/sign_up' => 'authorization#sign_up', as: 'sign_up'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
