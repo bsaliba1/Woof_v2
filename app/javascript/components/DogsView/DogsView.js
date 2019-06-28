c9128e8b5435aeb9a953adaf382c61c0fe7d7b79
Rui Min <ruimin.sim@gmail.com>
2019-06-28 00:48:20 -0400

    Debug a little for the DogsView js page

--- app/javascript/components/DogsView/DogsView.js
+++ app/javascript/components/DogsView/DogsView.js
@@ -6,20 +6,17 @@ import SittersList from "./SittersList/SittersList"
 class DogsView extends React.Component {
   render () {
     return (
-      <div className = "dogs-view">
-          <div class="title-label">
-    <p class="title">Sign up your dog now!</p>
-    <p class="title-sub">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    </p>
-  </div>
-          <DogCard/>
-          <DogCard/>
-          <DogCard/>
-          <DogCard/>
-          <DogCard/>
-          <DogCard/>
+        <div className = "dogs-view">
+        <div class = "title-label">
+            <p class ="title">Sign up your dog now!</p>
+            <p class ="title-sub">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod </p>
         </div>
+          <DogCard/>
+          <DogCard/>
+          <DogCard/>
+          <DogCard/>
+          <DogCard/>
+          <DogCard/>
       </div>
     );
   }
